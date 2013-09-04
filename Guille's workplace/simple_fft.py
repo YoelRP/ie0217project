5b281e77136d8a4307c87b74c586602a21dfbb2d
Guillermo Cornejo S <gmocornejos@gmail.com>
2013-09-04 17:49:23 -0600

    all working

--- Guille's workplace/simple_fft.py
+++ Guille's workplace/simple_fft.py
@@ -3,14 +3,15 @@ import cmath
 import scikits.audiolab as audio
 import matplotlib.pyplot as plt
 
+#from music to array
+(inputsignal, samplingrate, bits) = audio.oggread('Free_as_a_Bird.ogg')
+
+#fast furier transform using numpy
+transformedsignal = numpy.fft.fft(inputsignal)
+
+#plot using matlibplot
 norm = lambda x: cmath.polar(x)[0]
-(inputSignal, samplingRate, bits) = audio.oggread('Free_as_a_Bird.ogg')
-
-norm = lambda x: cmath.polar(x)[0]
-
-transformedSignal = numpy.fft.fft(signal)
-
-plt.plot([norm(x) for x in transformedSignal])
+plt.plot([norm(x) for x in transformedsignal])
 plt.show()
 
 #source http://jeremykun.com/2012/07/18/the-fast-fourier-transform/
